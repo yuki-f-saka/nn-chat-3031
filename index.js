759517cd646020eb20f8f73be8c0c22abfe4856c
yuki funasaka <yuki.funasaka.mithra@gmail.com>
2023-10-17 01:22:38 +0900

    renderで実行させるための対応

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const router = require('./lib/router');
 
 const basic = auth.basic({
   realm: 'Enter username and password.',
-  file: './users.htpasswd'
+  file: './users.htpasswd',
 });
 
-const server = http.createServer(basic.check((req, res) => {
-    router.route(req, res);
-  }))
-  .on('error', e => {
+const server = http
+  .createServer(
+    basic.check((req, res) => {
+      router.route(req, res);
+    })
+  )
+  .on('error', (e) => {
     console.error('Server Error', e);
   })
-  .on('clientError', e => {
+  .on('clientError', (e) => {
     console.error('Client Error', e);
   });
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.info(`Listening on ${port}`);
 });
